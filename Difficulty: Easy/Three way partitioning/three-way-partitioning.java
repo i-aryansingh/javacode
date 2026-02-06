d7902695981ffcbde7b71309e7f6a8be69733863
ig-aryansingh <aryan.singh.sde@gmail.com>
2026-02-06 21:14:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Three way partitioning/three-way-partitioning.java
@@ -0,0 +1,28 @@
+class Solution {
+    // Function to partition the array around the range such
+    // that array is divided into three parts.
+    public void threeWayPartition(int arr[], int a, int b) {
+        // code here
+         int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+            while(mid <= high) {
+            if(arr[mid] < a) {
+                int t = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = t;
+                low++;
+                mid++;
+            }
+                else if(arr[mid] > b) {
+                int t = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = t;
+                high--;
+            }
+            else {
+                mid++;
+            }
+        }
+    }
+}
